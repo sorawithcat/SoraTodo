1c2f14d47fb972ea5b23ffc7f1beaff00ca94843
sorawithcat <3153797616@qq.com>
2025-03-05 14:54:48 +0800

    修复折线图bug

--- Assets/Script/mainpanle/MainLineChartManager.cs
+++ Assets/Script/mainpanle/MainLineChartManager.cs
@@ -6,6 +6,12 @@ public class MainLineChartManager : MonoBehaviour
 {
     public LineChart chart;
 
+    private void Start()
+    {
+        //Todo:之后记得改成从save里获取
+        SetChartData(new List<double>() { });
+    }
+
     /// <summary>
     /// 设置折线数据
     /// </summary>
@@ -13,16 +19,14 @@ public class MainLineChartManager : MonoBehaviour
     public void SetChartData(List<double> data)
     {
         chart.ClearData();
-        if (data.Count < 7)
+        while (data.Count < 7 && data.Count >= 0)
         {
-            for (int i = 0; i <= 7 - data.Count; i++)
-            {
-                data.Add(0);
-            }
+            data.Add(0);
         }
+
         for (int i = 0; i < data.Count; i++)
         {
-            chart.AddXAxisData("x" + i);
+            chart.AddXAxisData(i.ToString());
             chart.AddData(0, data[i]);
         }
 
