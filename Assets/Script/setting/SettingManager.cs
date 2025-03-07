3edb4aaace983eb5c5f0e168815eefea353f204f
sorawithcat <3153797616@qq.com>
2025-03-07 19:16:18 +0800

    清理代码

--- Assets/Script/setting/SettingManager.cs
+++ Assets/Script/setting/SettingManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SettingManager : MonoBehaviour, ISaveManger
